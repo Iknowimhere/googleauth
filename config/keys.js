ef8554dd657b629b43a3a8b3cb9701de13bb53ed
Umashankar <104434797+Iknowimhere@users.noreply.github.com>
2023-11-17 05:54:57 +0000

    updated key.js

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,12 @@
 module.exports={
     google:{
-        clientID: "80823952974-iv5spi552emsj467bngv24mrljc5ni3d.apps.googleusercontent.com",
-        clientSecret: "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"
+        clientID: "",
+        clientSecret: ""
     },
     mongodb:{
-        mongodbURI:`mongodb://127.0.0.1:27017/umaDB`
+        mongodbURI:``
     },
     session:{
-        cookieKey:"umashankarwebdev"
+        cookieKey:""
     }
 }
